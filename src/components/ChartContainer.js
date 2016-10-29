e53831c187d0c5cf05ab4a699d8ce3618e580992
Yujin <yujinjcho@gmail.com>
2016-10-29 13:26:12 -0700

    Refactor data and formating between chart and container

--- src/components/ChartContainer.js
+++ src/components/ChartContainer.js
@@ -2,8 +2,26 @@ var React = require('react');
 var Row = require('react-bootstrap/lib/Row');
 var Col = require('react-bootstrap/lib/Col');
 var ChartInstance = require('./ChartInstance');
+var $ = require('jquery');
 
 var ChartContainer = React.createClass({
+  getInitialState: function() {
+    return {
+      daily: [
+        ['x', '01-01', '01-02', '01-03', '01-04', '01-05', '01-06', '01-07'],
+        ['daily', 2, 2, 3, 3, 4, 4, 5]
+      ],
+      cumulative: [
+        ['x', '01-01', '01-02', '01-03', '01-04', '01-05', '01-06', '01-07'],
+        ['daily', 2, 4, 7, 10, 14, 19, 24]
+      ]
+    };
+  },
+  loadData: function() {
+    $.ajax('/api/data').done(function(data) {
+      this.setState({data: data})
+    }.bind(this));
+  },
   render: function() {
     var chartRow = {
       flex: 2,
@@ -27,14 +45,20 @@ var ChartContainer = React.createClass({
     return (
       <Row className='show-grid' style={chartRow}>
         <Col style={chartContainer} xs={12}>
-            <ChartInstance title='Daily' />
+            <ChartInstance
+              title='Daily'
+              format={this.state.format}
+              data={this.state.daily}
+            />
             <div style={divider}></div>
-            <ChartInstance title='Cumulative' />
+            <ChartInstance
+              title='Cumulative'
+              data={this.state.cumulative}
+            />
         </Col>
       </Row>
     )
   }
 });
-
 
 module.exports = ChartContainer;
--- src/components/ChartInstance.js
+++ src/components/ChartInstance.js
@@ -1,31 +1,6 @@
 var React = require('react');
 import C3Chart from 'react-c3js';
 
-var data = {
-  x: 'x',
-  xFormat: '%m-%d',
-  columns: [
-    ['x', '01-01', '01-02', '01-03', '01-04', '01-05', '01-06', '01-07'],
-    ['daily', 5, 3, 10, 11, 5, 5, 22]
-  ],
-  types: {
-    daily: 'bar'
-  },
-  labels:true
-};
-var axis = {
-  x: {
-    type: 'timeseries',
-    localtime: false,
-    tick: {
-      format: '%m-%d'
-    }
-  }
-};
-var legend = {
-  hide: true
-};
-
 
 var ChartInstance = React.createClass({
   getInitialState: function() {
@@ -36,14 +11,41 @@ var ChartInstance = React.createClass({
         paddingTop: 15,
         width: '97%'
       },
-      titleName: {
+      chartTitle: {
         text: this.props.title
+      },
+      chartData: {
+        x: 'x',
+        xFormat: '%m-%d',
+        columns: this.props.data,
+        types: {
+          daily: 'bar'
+        },
+        labels:true
+      },
+      axis: {
+        x: {
+          type: 'timeseries',
+          localtime: false,
+          tick: {
+            format: '%m-%d'
+          }
+        }
+      },
+      legend: {
+        hide: true
       }
     }
   },
   render: function() {
     return (
-      <C3Chart style={this.state.chartStyle} title={this.state.titleName} data={data} legend={legend} axis={axis}/>
+      <C3Chart
+        style={this.state.chartStyle}
+        title={this.state.chartTitle}
+        data={this.state.chartData}
+        legend={this.state.legend}
+        axis={this.state.axis}
+      />
     )
   }
 });
