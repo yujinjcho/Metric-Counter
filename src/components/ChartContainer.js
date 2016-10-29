62cf8f1aa2b9f8b1dce259a5565206f4058aff00
Yujin <yujinjcho@gmail.com>
2016-10-28 23:14:29 -0700

    Add divider between charts

--- src/components/ChartContainer.js
+++ src/components/ChartContainer.js
@@ -7,7 +7,7 @@ var ChartContainer = React.createClass({
   render: function() {
     var chartRow = {
       flex: 2,
-      overflow: 'auto'
+      overflow: 'visible'
     };
 
     var chartContainer = {
@@ -17,10 +17,17 @@ var ChartContainer = React.createClass({
       height: '100%'
     };
 
+    var divider = {
+      border: '1px solid #CCCCCC',
+      height: 1,
+      width: '100%'
+    };
+
     return (
       <Row className='show-grid' style={chartRow}>
         <Col style={chartContainer} xs={12}>
             <ChartInstance title='Daily' />
+            <div style={divider}></div>
             <ChartInstance title='Cumulative' />
         </Col>
       </Row>
