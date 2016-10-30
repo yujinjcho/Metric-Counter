e349be4fab9b1c27acd86a41bec850e79782f23e
Yujin <yujinjcho@gmail.com>
2016-10-30 16:41:57 -0700

    Connect mongodb with express server

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,32 +1,48 @@
 
 var React = require('react');
 var Grid = require('react-bootstrap/lib/Grid');
-var Row = require('react-bootstrap/lib/Row');
-var Col = require('react-bootstrap/lib/Col');
+var $ = require('jquery');
+
 var ChartContainer = require('./ChartContainer');
 var ButtonContainer = require('./ButtonContainer');
 var NavbarInstance = require('./NavbarInstance');
 
+
 var Main = React.createClass({
   getInitialState: function() {
     return {
-      dailyPoints: ['daily', 2, 2, 3, 3, 4, 4, 10],
-      dailyDates: ['x', '01-01', '01-02', '01-03', '01-04', '01-05', '01-06', '01-07'],
-      cumulativePoints: ['daily', 2, 4, 5, 6, 7, 8, 10],
-      cumulativeDates: ['x', '01-01', '01-02', '01-03', '01-04', '01-05', '01-06', '01-07']
+      dateLabels: [],
+      dailyData: [],
+      cumulativeData: []
     }
   },
   componentDidMount: function() {
-    var newPoints = ['daily', 2, 2, 3, 3, 4, 4, 2];
-    var newDates = ['x', '01-01', '01-02', '01-03', '01-04', '01-05', '01-06', '01-07'];
-    var newCumulativePoints = ['daily', 2, 4, 5, 6, 7, 8, 15];
-    var newCumulativeDates = ['x', '01-01', '01-02', '01-03', '01-04', '01-05', '01-06', '01-07'];
-
-    this.setState({
-      dailyPoints: newPoints,
-      dailyDates: newDates,
-      cumulativePoints: newCumulativePoints,
-      cumulativeDates: newCumulativeDates
+    this.loadData();
+  },
+  loadData: function() {
+    $.ajax('/api/data').done(function(data){
+      this.setState({
+        dateLabels: data.dateLabels,
+        dailyData: data.dailyData,
+        cumulativeData: data.cumulativeData
+      });
+    }.bind(this));
+  },
+  addOne: function() {
+    var entry = {
+      'date': new Date()
+    };
+    $.ajax({
+      type: 'POST',
+      url: '/api/add',
+      contentType: 'application/json',
+      data: JSON.stringify(entry),
+      success: function(data) {
+        this.loadData();
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.log("Error adding entry: ", err);
+      }
     });
   },
   render: function() {
@@ -34,6 +50,7 @@ var Main = React.createClass({
       display: 'flex',
       flexFlow: 'column',
       height: '100%',
+      width: '100%',
       paddingLeft: 0,
       paddingRight: 0
     };
@@ -42,10 +59,9 @@ var Main = React.createClass({
       <Grid style={mainContainer}>
         <NavbarInstance />
         <ChartContainer
-          dailyPoints={this.state.dailyPoints}
-          dailyDates={this.state.dailyDates}
-          cumulativePoints={this.state.cumulativePoints}
-          cumulativeDates={this.state.cumulativeDates}
+          dateLabels={this.state.dateLabels}
+          dailyData={this.state.dailyData}
+          cumulativeData={this.state.cumulativeData}
         />
         <ButtonContainer />
       </Grid>
