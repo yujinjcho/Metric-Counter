e3b063c77fe1c14500a9c0050b2b32d072f21418
Yujin <yujinjcho@gmail.com>
2016-10-30 08:32:12 -0700

    Update dataflow to come from Main component
    Split data to reside in each charts initally because
    was having difficulty getting setState to update
    correctly within nested data structures, which a required
    format for the C3 component.
    
    Changed the column data to be stored in just arrays in
    a flat manner and created required structure in a new
    method. Added benefits will be allowing one AJAX
    call and only needing one ChartInstance component
    instead of separate for Daily and Cumulative charts.
    
    Also, will be good when syncing with submit button which
    will need to take a callback for updating the data
    of the parent component.

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,6 +8,27 @@ var ButtonContainer = require('./ButtonContainer');
 var NavbarInstance = require('./NavbarInstance');
 
 var Main = React.createClass({
+  getInitialState: function() {
+    return {
+      dailyPoints: ['daily', 2, 2, 3, 3, 4, 4, 10],
+      dailyDates: ['x', '01-01', '01-02', '01-03', '01-04', '01-05', '01-06', '01-07'],
+      cumulativePoints: ['daily', 2, 4, 5, 6, 7, 8, 10],
+      cumulativeDates: ['x', '01-01', '01-02', '01-03', '01-04', '01-05', '01-06', '01-07']
+    }
+  },
+  componentDidMount: function() {
+    var newPoints = ['daily', 2, 2, 3, 3, 4, 4, 2];
+    var newDates = ['x', '01-01', '01-02', '01-03', '01-04', '01-05', '01-06', '01-07'];
+    var newCumulativePoints = ['daily', 2, 4, 5, 6, 7, 8, 15];
+    var newCumulativeDates = ['x', '01-01', '01-02', '01-03', '01-04', '01-05', '01-06', '01-07'];
+
+    this.setState({
+      dailyPoints: newPoints,
+      dailyDates: newDates,
+      cumulativePoints: newCumulativePoints,
+      cumulativeDates: newCumulativeDates
+    });
+  },
   render: function() {
     var mainContainer = {
       display: 'flex',
@@ -20,7 +41,12 @@ var Main = React.createClass({
     return (
       <Grid style={mainContainer}>
         <NavbarInstance />
-        <ChartContainer />
+        <ChartContainer
+          dailyPoints={this.state.dailyPoints}
+          dailyDates={this.state.dailyDates}
+          cumulativePoints={this.state.cumulativePoints}
+          cumulativeDates={this.state.cumulativeDates}
+        />
         <ButtonContainer />
       </Grid>
     )
