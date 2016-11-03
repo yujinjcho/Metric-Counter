0d1ac20a7bd761d7b884511990bf953c231975be
Yujin <yujinjcho@gmail.com>
2016-11-03 15:44:56 -0700

    Change App title to The Metric Counter

--- src/components/NavbarInstance.js
+++ src/components/NavbarInstance.js
@@ -19,7 +19,7 @@ var NavbarInstance = React.createClass({
 
     return (
       <div className={this.state.navbarName} >
-          Metric Counter
+          The Metric Counter
           <div className="caret-container" onClick={this.handleClick}>
             <i className={this.state.caretName}></i>
           </div>
