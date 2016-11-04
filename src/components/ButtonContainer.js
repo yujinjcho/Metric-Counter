8dba4167f5fe0ce9cec9bb8672cff7c590366dc0
Yujin <yujinjcho@gmail.com>
2016-11-04 07:19:10 -0700

    Move button styling to stylesheet and add UI For press

--- src/components/ButtonContainer.js
+++ src/components/ButtonContainer.js
@@ -1,50 +1,38 @@
 var React = require('react');
 var Row = require('react-bootstrap/lib/Row');
 var Col = require('react-bootstrap/lib/Col');
-var update = require('react-addons-update');
+require('../../static/css/button-container.css');
 
 var ButtonContainer = React.createClass({
   getInitialState: function() {
     return {
-      buttonStyle: {
-        width: '98%',
-        background: '#0074D9',
-        height: 50,
-        fontSize: 20,
-        border: 'none',
-        color: 'white',
-        opacity: 0.8,
-        boxShadow :'0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)',
-        fontFamily: 'Arial',
-        zIndex: 5
-      }
+      buttonStyleName: 'button-style'
     }
   },
   render: function() {
-    var buttonRow = {
-      background: 'white',
-      height: 60,
-      textAlign: 'center'
-    };
-
     return (
-      <Row className='show-grid' style={buttonRow}>
-          <button style={this.state.buttonStyle} onClick={ this.handleClick }>
+      <Row className='show-grid button-row'>
+          <button className={this.state.buttonStyleName} onClick={ this.handleClick }>
             Submit
           </button>
       </Row>
     )
   },
   handleClick: function() {
-    this.changeButtonStyle(0.5);
-    setTimeout(function(){ this.changeButtonStyle(0.8) }.bind(this), 155);
+    this.changeButtonStyle();
+    setTimeout(function(){ this.changeButtonStyle() }.bind(this), 155);
     this.props.addOne();
   },
   changeButtonStyle: function(opacityLevel) {
-    this.setState({
-      buttonStyle: update(this.state.buttonStyle, {opacity: {$set: opacityLevel}})
-    });
-
+    if (this.state.buttonStyleName === 'button-style') {
+      this.setState({
+        buttonStyleName: 'button-style button-clicked'
+      });
+    } else {
+      this.setState({
+        buttonStyleName: 'button-style'
+      });
+    };
   }
 });
 
