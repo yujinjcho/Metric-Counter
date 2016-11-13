0b3a928d7d0e4df8ceaa002d2967357062b70ef1
achiu8 <achiu8@gmail.com>
2016-11-12 19:01:57 -0500

    Extract some more functions in pushups module

--- pushups.js
+++ pushups.js
@@ -10,10 +10,6 @@ var moment = require('moment');
 var GOAL = 5000;
 
 function renderLeaderboard(db, res, userData) {
-  var userIdNameMapping = userData.reduce(function(acc, item) {
-    return Object.assign(acc, { [item._id.userId]: item._id.name });
-  }, {});
-
   db.collection(config.mongoCollection).aggregate([
     {$match: {
       $or: [
@@ -32,41 +28,48 @@ function renderLeaderboard(db, res, userData) {
   ], function(err, data) {
     assert.equal(null, err);
 
-    var today = moment(new Date());
+    res.render('leaderboard', { users: usersSummary(data, userIdNameMapping(userData)) });
+  });
+}
 
-    var statsByUser = data.reduce(function(acc, item) {
-      var days = today.diff(moment(item.startDate), 'days') + 1;
-      var dailyAverage = Math.round(item.count / days * 10) / 10;
-      var projectedDaysLeft = Math.round((GOAL - item.count) / dailyAverage);
+function userIdNameMapping(userData) {
+  return userData.reduce(function(acc, item) {
+    return Object.assign(acc, { [item._id.userId]: item._id.name });
+  }, {});
+}
 
-      return Object.assign(
-        acc,
-        {
-          [item._id.userId]: {
-            count: item.count,
-            days: days,
-            dailyAverage: dailyAverage,
-            progress: Math.round(item.count / GOAL * 100) + '%',
-            projectedDaysLeft: projectedDaysLeft
-          }
+function usersSummary(userData, userIdNameMapping) {
+  var today = moment(new Date());
+
+  var statsByUser = userData.reduce(function(acc, item) {
+    var days = today.diff(moment(item.startDate), 'days') + 1;
+    var dailyAverage = Math.round(item.count / days * 10) / 10;
+    var projectedDaysLeft = Math.round((GOAL - item.count) / dailyAverage);
+
+    return Object.assign(
+      acc,
+      {
+        [item._id.userId]: {
+          count: item.count,
+          days: days,
+          dailyAverage: dailyAverage,
+          progress: Math.round(item.count / GOAL * 100) + '%',
+          projectedDaysLeft: projectedDaysLeft
         }
-      );
-    }, {})
+      }
+    );
+  }, {})
 
-    var results = Object.keys(userIdNameMapping).map(function(userId) {
+  return Object.keys(userIdNameMapping)
+    .map(function(userId) {
       return Object.assign(
         {},
         { name: userIdNameMapping[userId] },
         statsByUser[userId]
       );
-    });
-
-    var output = results
-      .filter(function(user) { return Number.isInteger(user.count) })
-      .sort(function(a, b) { return b.count - a.count });
-
-    res.render('leaderboard', { users: output });
-  });
+    })
+    .filter(function(user) { return Number.isInteger(user.count) })
+    .sort(function(a, b) { return b.count - a.count });
 }
 
 module.exports = {
