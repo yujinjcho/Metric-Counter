85618a0d663d56ea867d20adf187f7bd08e916c6
achiu8 <achiu8@gmail.com>
2016-11-16 19:23:23 -0500

    Use prop functions directly to handle clicks

--- src/components/ButtonContainer.js
+++ src/components/ButtonContainer.js
@@ -9,12 +9,13 @@ var ButtonContainer = React.createClass({
       buttonStyleName: 'button-style'
     }
   },
+
   render: function() {
     return (
       <Row className='show-grid button-row'>
           <div className={this.state.buttonStyleName}>
-            <button className='increment control' onClick={this.decrementCount}>Remove</button>
-            <button className='decrement control' onClick={this.incrementCount}>Add</button>
+            <button className='increment control' onClick={this.props.decrementCount}>Remove</button>
+            <button className='decrement control' onClick={this.props.incrementCount}>Add</button>
             <span>
               <input type="number" id='input-number' defaultValue='1' onKeyUp={this.handleInputKeyUp}/>
             </span>
@@ -27,14 +28,6 @@ var ButtonContainer = React.createClass({
     if (e.key === 'Enter') {
       this.props.incrementCount();
     }
-  },
-
-  incrementCount: function() {
-    this.props.incrementCount();
-  },
-
-  decrementCount: function() {
-    this.props.decrementCount();
   }
 });
 
