d62492bc136d75dd25d162f7084997d60a0a0220
Yujin <yujinjcho@gmail.com>
2016-11-24 09:24:10 -0800

    Moved all data handling to dataManager module

--- dataManager.js
+++ dataManager.js
@@ -3,10 +3,11 @@ var assert = require('assert');
 var moment = require('moment');
 
 function loadData(req, res, db) {
+  var start = startDate();
   if (req.user === undefined) {
-    guestData(res, startDate());
+    guestData(res, start);
   } else {
-    getDailyandTotalData(startDate(), res, req.user.id, req.query.category, db);
+    getDailyandTotalData(start, res, req.user.id, req.query.category, db);
   }
 };
 
@@ -140,12 +141,134 @@ function getDatesLabelInput() {
   return ['x'].concat(dates);
 };
 
+function loadCategories(req, res, db) {
+  if (req.user === undefined) {
+    var defaultCategory = {
+      categoriesByUpdate: [{_id: 'General', recent: new Date()}],
+      categoriesByCreated: []
+    };
+    res.json(defaultCategory);
+  } else {
+    recentCategories(req, res, db);
+  };
+};
+
+function recentCategories(req, res, db) {
+  db.collection(config.mongoCollection).aggregate([
+    {$match: {'userId': req.user.id}},
+    {$sort: {date:1}},
+    {$group: {
+      _id: '$category',
+      recent: {$last: '$date'}
+    }}
+  ], function(err, data) {
+    assert.equal(null, err);
+    userCategories(req, res, data, db);
+  });
+};
+
+function userCategories(req, res, categoriesLastUpdated, db) {
+  db.collection(config.mongoCategories).find({userId: req.user.id}).toArray(function(err, doc) {
+    var result = {
+      categoriesByCreated: doc,
+      categoriesByUpdate: categoriesLastUpdated
+    };
+    res.json(result);
+  })
+};
+
+function createCategory(req, res, db) {
+  if (req.user !== undefined) {
+    createCategoryInDB(res, req.user.id, req.params.category, db)
+  } else {
+    res.send('Must be logged in');
+  }
+};
+
+function deleteCategory(req, res, db) {
+  if (req.user !== undefined ) {
+    deleteCategoryInDB(res, req.user.id, req.params.category, db);
+  } else {
+    res.send('Must be logged in');
+  };
+}
+
+function createCategoryInDB(res, userId, category, db) {
+  db.collection(config.mongoCategories).insert({
+    userId: userId,
+    category: category,
+    date: new Date()
+  }, function(err, doc) {
+    res.send('Created successfully!');
+  });
+}
+
+function deleteCategoryInDB(res, userId, category, db) {
+  db.collection(config.mongoCategories).remove({
+    userId: userId,
+    category: category
+  }, function(err, result) {
+    assert.equal(err, null);
+    deleteCategoryMetrics(res, userId, category, db);
+  });
+};
+
+function deleteCategoryMetrics(res, userId, category, db) {
+  db.collection(config.mongoCollection).remove({
+    userId: userId,
+    category: category
+  }, function(err, result) {
+    assert.equal(err, null);
+    res.send('Delete Successful');
+  })
+};
+
+function handleUser(user, res, db) {
+  db.collection(config.mongoUsers).findOne({'userId': user.id}, function(err, doc) {
+    if (doc === null) {
+      console.log('Creating User');
+      createUser(user, res, db);
+    } else {
+      console.log('User found');
+      res.redirect('/');
+    }
+  });
+};
+
+function createUser(user, res, db) {
+  var userInfo = {
+    date: new Date(),
+    userId: user.id,
+    name: user.displayName
+  };
+  db.collection(config.mongoUsers).insert(userInfo, function(err, doc) {
+    createDefaultCategory(user.id, res, db);
+  });
+};
+
+function createDefaultCategory(userId, res, db) {
+  db.collection(config.mongoCategories).insert({
+    userId: userId,
+    category: 'General',
+    date: new Date()
+  }, function(err, doc) {
+    res.redirect('/');
+  });
+};
+
+
 function startDate() {
   return new Date(moment().subtract(6, 'days')
     .startOf('day').toISOString());
 };
 
+
+
 module.exports = {
   loadData: loadData,
-  addEntry: addEntry
+  addEntry: addEntry,
+  loadCategories: loadCategories,
+  createCategory: createCategory,
+  deleteCategory: deleteCategory,
+  handleUser: handleUser,
 }
--- webapp.js
+++ webapp.js
@@ -66,44 +66,16 @@ app.get('/leaderboard', function(req, res) {
 })
 
 app.get('/api/categories', function(req, res) {
-  loadCategories(req, res);
+  dataManager.loadCategories(req, res, db);
 });
-
-function loadCategories(req, res) {
-  if (req.user === undefined) {
-    var defaultCategory = {
-      categoriesByUpdate: [{_id: 'General', recent: new Date()}],
-      categoriesByCreated: []
-    };
-    res.json(defaultCategory);
-  } else {
-    recentCategories(req, res);
-  };
-};
 
 app.post('/api/categories/:category', function(req, res) {
-  createCategory(req, res);
+  dataManager.createCategory(req, res, db);
 });
 
-function createCategory(req, res) {
-  if (req.user !== undefined) {
-    createCategoryInDB(res, req.user.id, req.params.category)
-  } else {
-    res.send('Must be logged in');
-  }
-};
-
 app.delete('/api/categories/:category', function(req, res) {
-  deleteCategory(req, res);
+  dataManager.deleteCategory(req, res, db);
 })
-
-function deleteCategory(req, res) {
-  if (req.user !== undefined ) {
-    deleteCategoryInDB(res, req.user.id, req.params.category);
-  } else {
-    res.send('Must be logged in');
-  };
-}
 
 app.post('/api/add', function(req, res) {
   dataManager.addEntry(req, res, db);
@@ -121,96 +93,9 @@ app.get(
   '/login/return',
   passport.authenticate('facebook', { session: true, failureRedirect: '/' }),
   function(req, res) {
-    handleUser(req.user, res);
+    dataManager.handleUser(req.user, res, db);
   }
 );
-
-function createCategoryInDB(res, userId, category) {
-  db.collection(config.mongoCategories).insert({
-    userId: userId,
-    category: category,
-    date: new Date()
-  }, function(err, doc) {
-    res.send('Created successfully!');
-  });
-}
-
-function deleteCategoryInDB(res, userId, category) {
-  db.collection(config.mongoCategories).remove({
-    userId: userId,
-    category: category
-  }, function(err, result) {
-    assert.equal(err, null);
-    deleteCategoryMetrics(res, userId, category);
-  });
-};
-
-function deleteCategoryMetrics(res, userId, category) {
-  db.collection(config.mongoCollection).remove({
-    userId: userId,
-    category: category
-  }, function(err, result) {
-    assert.equal(err, null);
-    res.send('Delete Successful');
-  })
-};
-
-function recentCategories(req, res) {
-  db.collection(config.mongoCollection).aggregate([
-    {$match: {'userId': req.user.id}},
-    {$sort: {date:1}},
-    {$group: {
-      _id: '$category',
-      recent: {$last: '$date'}
-    }}
-  ], function(err, data) {
-    assert.equal(null, err);
-    userCategories(req, res, data);
-  });
-};
-
-function userCategories(req, res, categoriesLastUpdated) {
-  db.collection(config.mongoCategories).find({userId: req.user.id}).toArray(function(err, doc) {
-    var result = {
-      categoriesByCreated: doc,
-      categoriesByUpdate: categoriesLastUpdated
-    };
-    res.json(result);
-  })
-};
-
-function handleUser(user, res) {
-  db.collection(config.mongoUsers).findOne({'userId': user.id}, function(err, doc) {
-    if (doc === null) {
-      console.log('Creating User');
-      createUser(user, res);
-    } else {
-      console.log('User found');
-      res.redirect('/');
-    }
-  });
-};
-
-function createUser(user, res) {
-  var userInfo = {
-    date: new Date(),
-    userId: user.id,
-    name: user.displayName
-  };
-  db.collection(config.mongoUsers).insert(userInfo, function(err, doc) {
-    createDefaultCategory(user.id, res);
-  });
-};
-
-function createDefaultCategory(userId, res) {
-  db.collection(config.mongoCategories).insert({
-    userId: userId,
-    category: 'General',
-    date: new Date()
-  }, function(err, doc) {
-    res.redirect('/');
-  });
-};
 
 MongoClient.connect(config.mongoDev, function(err, dbConnection) {
   db = dbConnection;
