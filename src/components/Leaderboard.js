b6b0a4973d35e597cb00912026673beec6e2e09f
achiu8 <achiu8@gmail.com>
2016-12-16 21:07:06 -0500

    Create Leaderboard component

---
+++ src/components/Leaderboard.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import $ from 'jquery';
+
+export default class Leaderboard extends React.Component {
+  constructor() {
+    super();
+    this.state = { users: [] };
+  }
+
+  componentDidMount() {
+    $.get('/api/leaderboard', ({ users }) => {
+      this.setState({ users })
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Leaderboard</h1>
+        <table>
+          {this._renderHeading()}
+          {this._renderBody()}
+        </table>
+      </div>
+    );
+  }
+
+  _renderHeading() {
+    return (
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Count</th>
+          <th>Days</th>
+          <th>Average</th>
+          <th>Progress</th>
+          <th>Days Left</th>
+        </tr>
+      </thead>
+    );
+  }
+
+  _renderBody() {
+    return <tbody>{this.state.users.map(this._renderUser)}</tbody>;
+  }
+
+  _renderUser({ name, count, days, dailyAverage, progress, projectedDaysLeft }, i) {
+    return (
+      <tr key={i}>
+        <td>{name}</td>
+        <td>{count}</td>
+        <td>{days}</td>
+        <td>{dailyAverage}</td>
+        <td>{progress}</td>
+        <td>{projectedDaysLeft}</td>
+      </tr>
+    );
+  }
+}
