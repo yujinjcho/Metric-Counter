41008c3a573efcc894721b44ed4afa04b391042e
Yujin <yujinjcho@gmail.com>
2016-12-25 06:46:58 -0800

    Change function declarations to arrow functions

--- webapp.js
+++ webapp.js
@@ -25,18 +25,13 @@ passport.use(
     clientID: config.fbAppId,
     clientSecret: config.fbSecret,
     callbackURL: '/login/return'},
-    function(accessToken, refreshToken, profile, cb) {
-      return cb(null, profile);
-    }
+    (accessToken, refreshToken, profile, cb) => cb(null, profile)
   )
 );
 
-passport.serializeUser(function(user, cb) {
-  cb(null, user);
-});
-passport.deserializeUser(function(obj, cb) {
-  cb(null, obj);
-});
+passport.serializeUser((user, cb) => cb(null, user));
+passport.serializeUser((user, cb) => cb(null, user));
+passport.deserializeUser((obj, cb) => cb(null, obj));
 
 app.use(session({
   secret: config.expressSecret,
@@ -49,64 +44,56 @@ app.use(passport.session());
 app.use(express.static('dist'));
 app.use(bodyParser.json());
 
-app.get('/api/data', function(req, res) {
-  dataManager.loadData(req, res, db);
-});
+app.get('/api/data', (req, res) => dataManager.loadData(req, res, db));
 
-app.get('/leaderboard', function(req, res) {
-  res.render('leaderboard');
-});
+app.get('/leaderboard', (req, res) => res.render('leaderboard'));
 
-app.get('/api/leaderboard', function(req, res) {
-  db.collection(config.mongoUsers).aggregate([
-      {$group: {
-        _id: {
-          userId: '$userId',
-          name: '$name'
-        }
-      }}
-    ], function(err, data) {
-      pushups.leaderboardData(db, res, data);
-    }
+app.get('/api/leaderboard', (req, res) => {
+  db.collection(config.mongoUsers).aggregate(
+    [{$group: {
+      _id: {
+        userId: '$userId',
+        name: '$name'
+      }
+    }}],
+    (err, data) => pushups.leaderboardData(db, res, data)
   );
 });
 
-app.get('/api/categories', function(req, res) {
-  dataManager.loadCategories(req, res, db);
-});
+app.get('/api/categories', (req, res) => dataManager.loadCategories(req, res, db));
 
-app.post('/api/categories/:category', function(req, res) {
-  dataManager.createCategory(req, res, db);
-});
+app.post(
+  '/api/categories/:category',
+  (req, res) => dataManager.createCategory(req, res, db)
+);
 
-app.delete('/api/categories/:category', function(req, res) {
-  dataManager.deleteCategory(req, res, db);
-});
+app.delete(
+  '/api/categories/:category',
+  (req, res) => dataManager.deleteCategory(req, res, db)
+);
 
-app.post('/api/add', function(req, res) {
-  dataManager.addEntry(req, res, db);
-});
+app.post(
+  '/api/add',
+  (req, res) => dataManager.addEntry(req, res, db)
+);
 
 app.get('/login', passport.authenticate('facebook'));
 
-app.get('/logout', function(req, res) {
-  req.session.destroy(function(err) {
-    res.redirect('/');
-  });
-});
+app.get(
+  '/logout',
+  (req, res) => {req.session.destroy((err) => { res.redirect('/') })}
+);
 
 app.get(
   '/login/return',
   passport.authenticate('facebook', {session: true, failureRedirect: '/'}),
-  function(req, res) {
-    dataManager.handleUser(req.user, res, db);
-  }
+  (req, res) => dataManager.handleUser(req.user, res, db)
 );
 
-MongoClient.connect(config.mongoDev, function(err, dbConnection) {
+MongoClient.connect(config.mongoDev, (err, dbConnection) => {
   db = dbConnection;
-  var server = app.listen(config.port, function() {
-    var port = server.address().port;
+  let server = app.listen(config.port, () => {
+    let port = server.address().port;
     console.log('Started server at port:', port);
   });
 });
